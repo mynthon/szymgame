0c97f36318d733184d118a46bfc57b61a04a35fa
Krzysztof Grzembski <krzysztof.grzembski@netmedia.com.pl>
2014-03-27 08:30:04 +0100

    merge conflict

--- sz/app/pi/map.js
+++ sz/app/pi/map.js
@@ -1,9 +1,11 @@
 Sz_App_Pi_Map = Sz_Engine_Quark_Dynamic.extend({
 	onAdd: function(){
+		this.maze = new Sz_App_Pi_Maze_Classic()
+		this.maze.init(41,41)
 		this.$ = $('<div style="width:200px; height:200px; border:1px solid #eee; position:relative;" />')
 		this._root.$.append(this.$)
 		this.generator = new Sz_App_Pi_Maze_Classic()
-		this.generator.init(21, 21)
+		this.generator.init(41, 41)
 	},
 	
 	onFrame: function(){
@@ -12,11 +14,11 @@ Sz_App_Pi_Map = Sz_Engine_Quark_Dynamic.extend({
 	
 	drawMaze: function(){
 		var map = this.generator.map
-		var w = 32
-		var h = 32
+		var w = 2
+		var h = 2
 		
 		for (var i = 0; i < map.length; i++){
-			for (var j = 0; j < map[i].length; j++){
+			for (var j = 0; j < map[i].lenght; j++){
 				if (map[i][j] === 0){
 					var left = j*w
 					var top = i*h
@@ -25,13 +27,12 @@ Sz_App_Pi_Map = Sz_Engine_Quark_Dynamic.extend({
 					div.css({
 						'top': top,
 						'left': left,
-						'position': 'absolute',
+						'position': absolute,
 						'width': w,
-						'height': h,
-						'background': 'purple'
+						'height': h
 					})
 					
-					this.$.append(div)
+					$this.$.append(div)
 					
 				}
 			}
